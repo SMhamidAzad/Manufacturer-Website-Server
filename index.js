c83c3938feaefc48cef5fac1e903095592e3a185
SMhamidAzad <hamidazad020@gmail.com>
2022-05-24 20:14:39 +0600

    two read operation done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -8,15 +9,33 @@ app.use(cors());
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.roc0q.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  console.log('db is connected');
-  client.close();
-});
+
+async function run() {
+  try {
+    await client.connect();
+
+    // all collection 
+    const toolsCollection = client.db('toolsManufacturer').collection('tools');
+
+    app.get('/tools', async (req, res) => {
+      const query = {};
+      const result = await toolsCollection.find(query).toArray();
+      res.send(result)
+    })
+    app.get('/tools/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await toolsCollection.findOne(query);
+      res.send(result)
+    })
+  }
+  finally {
+
+  }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
