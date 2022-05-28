88070fe27fa73374cef1342ce76b9645b866095a
SMhamidAzad <hamidazad020@gmail.com>
2022-05-28 10:33:12 +0600

    delete user btn done

--- index.js
+++ index.js
@@ -71,6 +71,12 @@ async function run() {
         res.send(result)
       }
     })
+    app.delete('/orders/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.send(result);
+    });
 
 
     // review api 
@@ -88,6 +94,13 @@ async function run() {
     app.get('/user', async (req, res) => {
       const allUser = await userCollection.find().toArray();
       res.send(allUser);
+    });
+
+    app.delete('/user/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
     });
 
 
